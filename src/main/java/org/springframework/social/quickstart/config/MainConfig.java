bb02b2450c3d1bf8697025d14303324b02c7e707
thomasfl <thomas.flemming@gmail.com>
2013-03-13 10:45:40 +0100

    change db from h2 to postgres

--- src/main/java/org/springframework/social/quickstart/config/MainConfig.java
+++ src/main/java/org/springframework/social/quickstart/config/MainConfig.java
@@ -15,8 +15,12 @@
  */
 package org.springframework.social.quickstart.config;
 
+import java.net.URI;
+
+import javax.management.RuntimeErrorException;
 import javax.sql.DataSource;
 
+import org.postgresql.jdbc3.Jdbc3PoolingDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
@@ -40,13 +44,44 @@ import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 public class MainConfig {
 
 	@Bean(destroyMethod = "shutdown")
-	public DataSource dataSource() {
+	public DataSource dataSource_original() {
 		EmbeddedDatabaseFactory factory = new EmbeddedDatabaseFactory();
 		factory.setDatabaseName("spring-social-quickstart");
 		factory.setDatabaseType(EmbeddedDatabaseType.H2);
 		factory.setDatabasePopulator(databasePopulator());
 		return factory.getDatabase();
 	}
+
+	@Bean
+    public DataSource dataSource() throws Exception {
+		String databaseUrl = System.getenv("DATABASE_URL");
+		if(databaseUrl == null){
+			databaseUrl = System.getenv("LOCAL_BOLIGER_DATABASE_URL");
+		}
+		if(databaseUrl == null){
+			throw new RuntimeErrorException(null, "Missing environment variable DATABASE_URL");
+		}
+        URI dbUri = new URI(databaseUrl);
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        
+        Jdbc3PoolingDataSource source = new Jdbc3PoolingDataSource();
+        source.setDataSourceName("PostgreSQL Data Source");
+        source.setServerName(dbUri.getHost());
+        source.setDatabaseName(dbUri.getPath());
+        
+        source.setUser(username);
+        source.setPassword(password);
+        source.setMaxConnections(10);
+        return source;
+//        
+//        DataSource basicDataSource = new DataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);        
+//        return basicDataSource;
+    }
 
 	// internal helpers
 
